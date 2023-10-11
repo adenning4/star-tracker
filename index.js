42f3f006d218d09774acbd00452f561c982608ea
adenning4 <41348927+adenning4@users.noreply.github.com>
2023-10-11 13:44:02 -0600

    Add cardinal direction descriptor

--- index.js
+++ index.js
@@ -87,8 +87,48 @@ function displayLiveCoordinates(altAzTimeCurveArray) {
     dataTimestampEl.textContent = timestampDate.toTimeString();
     altitudeResultEl.textContent = altAzTimeCurveArray[i].alt;
     azimuthResultEl.textContent = altAzTimeCurveArray[i].az;
+    console.log(getAzimuthCardinalDirections(altAzTimeCurveArray[i].az));
     i++;
   }, 1000);
+}
+
+function getAzimuthCardinalDirections(azimuthDegrees) {
+  if (
+    (azimuthDegrees >= 0 && azimuthDegrees < 11.25) ||
+    (azimuthDegrees >= 348.75 && azimuthDegrees <= 360)
+  ) {
+    return "N";
+  } else if (azimuthDegrees >= 11.25 && azimuthDegrees < 33.75) {
+    return "NNE";
+  } else if (azimuthDegrees >= 33.75 && azimuthDegrees < 56.25) {
+    return "NE";
+  } else if (azimuthDegrees >= 56.25 && azimuthDegrees < 78.75) {
+    return "ENE";
+  } else if (azimuthDegrees >= 78.75 && azimuthDegrees < 101.25) {
+    return "E";
+  } else if (azimuthDegrees >= 101.25 && azimuthDegrees < 123.75) {
+    return "ESE";
+  } else if (azimuthDegrees >= 123.75 && azimuthDegrees < 146.25) {
+    return "SE";
+  } else if (azimuthDegrees >= 146.25 && azimuthDegrees < 168.75) {
+    return "SSE";
+  } else if (azimuthDegrees >= 168.75 && azimuthDegrees < 191.25) {
+    return "S";
+  } else if (azimuthDegrees >= 191.25 && azimuthDegrees < 213.75) {
+    return "SSW";
+  } else if (azimuthDegrees >= 213.75 && azimuthDegrees < 236.25) {
+    return "SW";
+  } else if (azimuthDegrees >= 236.25 && azimuthDegrees < 258.75) {
+    return "WSW";
+  } else if (azimuthDegrees >= 258.75 && azimuthDegrees < 281.25) {
+    return "W";
+  } else if (azimuthDegrees >= 281.25 && azimuthDegrees < 303.75) {
+    return "WNW";
+  } else if (azimuthDegrees >= 303.75 && azimuthDegrees < 326.25) {
+    return "NW";
+  } else if (azimuthDegrees >= 326.25 && azimuthDegrees < 348.75) {
+    return "NNW";
+  }
 }
 
 function getRequestUrl(coordinates, trackingObject) {
