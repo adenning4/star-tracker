409e6d1bbefb24919ce2de6abef51be1f64d704f
adenning4 <41348927+adenning4@users.noreply.github.com>
2023-10-18 21:31:59 -0600

    Adding vite config

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+export default defineConfig({
+  optimizeDeps: {
+    include: ["linked-dep"],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/linked-dep/, /node_modules/],
+    },
+  },
+});
